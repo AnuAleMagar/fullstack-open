99d35ff479978970c448bc9e70a13bc9897cb242
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-30 18:52:11 +0545

    1.3-1.4 completed

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Part from "./Part";
-const Content = (props) => {
+const Content = ({parts}) => {
+  console.log(parts)
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={parts[0].name} exercise={parts[0].exercises} />
+      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </>
   );
 };
