fc0155342eda6686efab58c6bcdd04ff18a14ca8
Anu Ale Magar <anualemagar212@gmail.com>
2025-08-04 20:57:47 +0545

    part2 upto 2.5 completed

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
-function Course({course}) {
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+function Course({ courses }) {
   return (
-    <div key={course.id}>
-        <Header heading={course.name}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts} />
+    <div><h1>Web Development Curriculum</h1>
+      {courses.map((course) => {
+        return (<div key={course.id}>
+          <Header heading={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>)
+      })}
     </div>
-  )
+  );
 }
 
-export default Course
+
+export default Course;
--- part2/courseinfo/src/Header.jsx
+++ part2/courseinfo/src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.heading}</h1>
+      <h2>{props.heading}</h2>
     </div>
   );
 };
