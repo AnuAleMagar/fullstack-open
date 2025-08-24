dae178aed9001dbd9fbdb64f61bbaa9e1c9146f7
Anu Ale Magar <anualemagar212@gmail.com>
2025-08-24 18:47:53 +0545

    3.3 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,39 +1,53 @@
-const express=require('express');
-const app=express();
-const PORT=3001;
+const express = require("express");
+const app = express();
+const PORT = 3001;
 app.use(express.json());
 
-const persons=[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
-app.get('/api/persons',(request,response)=>{
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
+app.get("/api/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `Phonebook has info for ${
+      persons.length
+    } people <br/> <br/> ${date.toString()}`
+  );
+});
 
-app.get('/api/info',(request,response)=>{
-    const date=new Date();
-    response.send(`Phonebook has info for ${persons.length} people <br/> <br/> ${date.toString()}`)
-})
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+app.get("/api/persons/:id", (request, response) => {
+  const idd = request.params.id;
+  const person = persons.find((per) => per.id === idd);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
