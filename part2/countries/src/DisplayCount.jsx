ced60a6893428135286103f922d978ef628a37b0
Anu Ale Magar <anualemagar212@gmail.com>
2025-08-24 12:07:53 +0545

    2.19 completed

--- part2/countries/src/DisplayCount.jsx
+++ part2/countries/src/DisplayCount.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SingleCountry from './SingleCountry'
 function DisplayCount({data}) {
+    const [index,setIndex]=useState(null)
+    function handleClick(ind){
+        setIndex(ind)
+    }
   return (
     <div>
         {data.length===1?
         <SingleCountry data={data} />:<ul>
             {data.map((country,ind)=>{
                 return (
-                    <li key={ind}>{country.name.common}</li>
+                    <>
+                    <li key={ind}>{country.name.common} <button onClick={()=>handleClick(ind)}>Show</button></li>
+                    {ind===index && <SingleCountry data={[country]} />} 
+                    </>
                 )
             })}
         </ul>}
