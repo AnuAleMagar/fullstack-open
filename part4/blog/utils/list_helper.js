758260eec44f1a7719cfb86b0f9cb269e3758e6a
Anu Ale Magar <anualemagar212@gmail.com>
2025-09-07 15:41:31 +0545

    4.4 completed

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,4 +1,14 @@
 const dummy=(blogs)=>{
  return 1
 }
-module.exports={dummy}
+const TotalLikes=(blogs)=>{
+ if(blogs.length===0)return 0
+ if(blogs.length===1)return blogs[0].likes;
+ let sum=0;
+ for(let blog of blogs){
+    sum+=blog.likes;
+ }
+ return sum;
+}
+module.exports={dummy,TotalLikes}
+
