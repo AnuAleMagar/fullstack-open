c3620651827712e0085876cb0a26b4c11e7c8053
Anu Ale Magar <anualemagar212@gmail.com>
2025-09-17 13:33:38 +0545

    4.23 completed

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -4,15 +4,28 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
-const helper=require('./test_helper')
+const helper = require("./test_helper");
 const api = supertest(app);
-
+let token;
 beforeEach(async () => {
   await Blog.deleteMany({});
   let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
   blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
+
+  await api.post("/api/users").send({
+    username: "test",
+    name: "Test",
+    password: "password",
+  });
+
+  const loginResponse = await api.post("/api/login").send({
+    username: "test",
+    password: "password",
+  });
+
+  token = loginResponse.body.token;
 });
 
 test("blogs are returned as json", async () => {
@@ -41,21 +54,36 @@ test("http post request", async () => {
   };
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
   const responseAgain = await api.get("/api/blogs");
   assert.strictEqual(responseAgain.body.length, helper.initialBlogs.length + 1);
 });
+test("if a token is not provided,a blog fails with the proper status code 401 Unauthorized  ", async () => {
+  const newBlogWithLikeMissing = {
+    title: "Testing for blog without token passing in header",
+    author: "Fullstack",
+    url: "https://example.com/guide-to-mongodb",
+    likes:100,
+  };
+  const response = await api
+    .post("/api/blogs")
+    .send(newBlogWithLikeMissing)
+    .expect(401)
+    .expect("Content-Type", /application\/json/);
+  assert.ok(response.body.error.includes("token invalid"));
+});
 test("if likes property is missing from the request,it will default to value 0", async () => {
   const newBlogWithLikeMissing = {
     title: "Testing for blog with like missing",
     author: "Fullstack",
     url: "https://example.com/guide-to-mongodb",
-    
   };
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .send(newBlogWithLikeMissing)
     .expect(201)
     .expect("Content-Type", /application\/json/);
@@ -65,49 +93,56 @@ test("if title or url property are missing from the request,it will respondes wi
   const newBlogWithTitleMissing = {
     author: "Fullstack",
     url: "https://example.com/guide-to-mongodb",
-    
   };
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
     .send(newBlogWithTitleMissing)
     .expect(400)
     .expect("Content-Type", /application\/json/);
-
 });
 
+test("a blog can be deleted", async () => {
+  const newBlogTobeDeleted = {
+    title: "A Guide from testing",
+    author: "Bob Johnson",
+    url: "https://example.com/guide-to-mongodb",
+    likes: 88,
+  };
+  const createdBlog = await api
+    .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
+    .send(newBlogTobeDeleted)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
 
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
-
+  const blogToDelete = createdBlog.body;
   await api
     .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+    .set("Authorization", `Bearer ${token}`)
+    .expect(204);
 
-  const blogsAtEnd = await helper.blogsInDb()
+  const blogsAtEnd = await helper.blogsInDb();
 
-  const titles = blogsAtEnd.map(n => n.title)
-  assert(!titles.includes(blogToDelete.content))
+  const titles = blogsAtEnd.map((n) => n.title);
+  assert(!titles.includes(blogToDelete.title));
+});
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
-})
-
-test('a blog can be updated', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToUpdate = blogsAtStart[0]
-  const updatedData={likes:blogToUpdate.likes+100}
+test("a blog can be updated", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToUpdate = blogsAtStart[0];
+  const updatedData = { likes: blogToUpdate.likes + 100 };
   await api
     .put(`/api/blogs/${blogToUpdate.id}`)
+    .set("Authorization", `Bearer ${token}`)
     .send(updatedData)
-    .expect(200)
+    .expect(200);
 
+  const blogsUpdated = await helper.blogsInDb();
+  const updatedBlog = blogsUpdated.find((blog) => blog.id === blogToUpdate.id);
 
-  const blogsUpdated = await helper.blogsInDb()
-  const updatedBlog=blogsUpdated.find(blog=>blog.id===blogToUpdate.id)
-
-
-  assert.strictEqual(updatedBlog.likes,updatedData.likes)
-})
+  assert.strictEqual(updatedBlog.likes, updatedData.likes);
+});
 
 after(async () => {
   await mongoose.connection.close();
