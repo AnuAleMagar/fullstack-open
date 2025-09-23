6cb3e5fc56c82918aae6f228e9c34c51d0590487
Anu Ale Magar <anualemagar212@gmail.com>
2025-09-23 12:14:32 +0545

    5.2 completed

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -7,22 +7,26 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-  const handleLogin = async event => {
-    event.preventDefault()
-    
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
     try {
-      const user = await loginService.login({ username, password })
-      setUser(user)
-      setUsername('')
-      setPassword('')
-      console.log(user)
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
     } catch {
-      setErrorMessage('wrong credentials')
+      setErrorMessage("wrong credentials");
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+        setErrorMessage(null);
+      }, 5000);
     }
+  };
+  function handleLogout() {
+    window.localStorage.removeItem("loggedUser");
+    setUser(null)
   }
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -52,6 +56,8 @@ const App = () => {
   );
 
   useEffect(() => {
+    const loggedUser = JSON.parse(window.localStorage.getItem("loggedUser"));
+    setUser(loggedUser);
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
@@ -63,7 +69,10 @@ const App = () => {
         <>
           {" "}
           <h2>blogs</h2>
-          <p>{user.username} logged in</p>
+          <p>
+            {user.username} logged in{" "}
+            <button onClick={handleLogout}>logout</button>
+          </p>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
