47ef53adcb4249b4a95026cc25adf66d8c51a5f8
Anu Ale Magar <anualemagar212@gmail.com>
2025-11-18 17:19:47 +0545

    13.21 completed

--- part13/controllers/users.js
+++ part13/controllers/users.js
@@ -9,31 +9,50 @@ const userFinder = async (req, res, next) => {
 router.get("/", async (req, res) => {
   const users = await User.findAll({
     include: {
-      model: Blog
-    }
+      model: Blog,
+    },
   });
   res.json(users);
 });
 
 router.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id, {
-    attributes: { exclude: ["createdAt", "updatedAt"] }, 
+    attributes: { exclude: ["createdAt", "updatedAt"] },
     include: [
       {
         model: Blog,
-        as: "readings", 
-        attributes: { exclude: ["createdAt", "updatedAt", "userId"] }, 
-      }
-    ]
+        as: "readings",
+        attributes: { exclude: ["createdAt", "updatedAt", "userId"] },
+        through: {
+          attributes: ["read", "id"],
+        },
+      },
+    ],
   });
 
   if (!user) {
     return res.status(404).json({ error: "User not found" });
   }
 
-  res.json(user);
-});
+  const userJson = user.toJSON();
+  if (userJson.readings) {
+    userJson.readings = userJson.readings.map((blog) => {
+      const joinData =
+        blog.reading_list || blog.ReadingList || blog.readinglists;
 
+      const { reading_list, ReadingList, ...blogData } = blog;
+
+      return {
+        ...blogData,
+        readinglists: joinData
+          ? [{ read: joinData.read, id: joinData.id }]
+          : [],
+      };
+    });
+  }
+
+  res.json(userJson);
+});
 router.post("/", async (req, res) => {
   console.log(req.body);
   const user = await User.create(req.body);
@@ -43,7 +62,7 @@ router.post("/", async (req, res) => {
 router.put("/:username", userFinder, async (req, res) => {
   if (req.user) {
     req.user.username = req.body.username;
-    await req.user.save();  
+    await req.user.save();
     res.json(req.user);
   } else {
     return res.status(404).json({ error: "User not found" });
