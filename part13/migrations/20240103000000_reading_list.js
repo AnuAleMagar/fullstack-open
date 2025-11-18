054cfb3d7494049b0f24fa3f1240c87ff427c15a
Anu Ale Magar <anualemagar212@gmail.com>
2025-11-18 17:10:24 +0545

    13.19 completed

---
+++ part13/migrations/20240103000000_reading_list.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("reading_lists", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "users", key: "id" },
+      },
+      blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "blogs", key: "id" },
+      },
+      read: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+
+    // Add unique constraint to prevent duplicate entries
+    await queryInterface.addConstraint("reading_lists", {
+      fields: ["user_id", "blog_id"],
+      type: "unique",
+      name: "unique_user_blog",
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("reading_lists");
+  },
+};
--- part13/models/index.js
+++ part13/models/index.js
@@ -1,11 +1,21 @@
-const Blog =require('./blog')
-const User =require('./user')
-
+const Blog = require("./blog");
+const User = require("./user");
+const ReadingList = require("./reading_list");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.belongsToMany(Blog, {
+  through: ReadingList,
+  as: "readings", 
+});
+Blog.belongsToMany(User, {
+  through: ReadingList,
+  as: "reading_users", 
+});
+
 module.exports = {
   Blog,
-  User
-}
+  User,
+  ReadingList,
+};
