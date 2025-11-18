3c65c9a761c05861dcf27c6631f6d1ce62571d86
Anu Ale Magar <anualemagar212@gmail.com>
2025-11-18 17:00:56 +0545

    13.18 completed

--- part13/models/blog.js
+++ part13/models/blog.js
@@ -1,34 +1,57 @@
-const { Model, DataTypes } = require('sequelize')
+const { Model, DataTypes } = require("sequelize");
 
-const { sequelize } = require('../util/db')
+const { sequelize } = require("../util/db");
 
 class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1991,
+        max: new Date().getFullYear(),
+        isInt: {
+          msg: "Year must be an integer",
+        },
+        customValidator(value) {
+          const currentYear = new Date().getFullYear();
+          if (value < 1991) {
+            throw new Error("Year must be at least 1991");
+          }
+          if (value > currentYear) {
+            throw new Error(`Year cannot be greater than ${currentYear}`);
+          }
+        },
+      },
+    },
   },
-  author: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue:0
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "blog",
   }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: true,
-  modelName: 'blog'
-})
-module.exports = Blog
+);
+module.exports = Blog;
